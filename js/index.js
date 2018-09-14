d9568ad63d60f5c1e08411489466976276e8165d
“anuragMaravi” <anuragmaravi12@gmail.com>
2018-09-14 18:57:21 -0400

    “Download_config”

--- js/index.js
+++ js/index.js
@@ -42,27 +42,50 @@ function setStateNames() {
 
 //Transitions
 function setTransition() {
+  var duplicate = false;
   var stateName = document.getElementsByName('state')[0].value;
   var eventName = document.getElementsByName('event')[0].value;
   var nextState = document.getElementsByName('nextState')[0].value;
   var direction = document.getElementsByName('direction')[0].value;
 
+  if(statesJArray.length > 0) {
+    for(var i = 0; i < statesJArray.length; i++) {
+        var obj = statesJArray[i];
+        if(obj.stateName == stateName) {
+          var tArray = obj.transition;
+          for(var i = 0; i < tArray.length; i++) {
+            var tObj = tArray[i];
+            if(tObj.event != eventName) {
+              var transitionObj = new Object();
+              transitionJArray = [];
+              transitionObj.event = eventName;
+              transitionObj.nextState = nextState;
+              transitionObj.direction = direction;
+              tArray.push(transitionObj);
+          } }
+          console.log(obj.transition);
+         duplicate = true;
+        } 
+    }
+  }
   if(stateName != "" || eventName != "" || nextState != "" || direction != ""){
     transitions.push(" State: " + stateName + ", Event: " + eventName +", Next State: " + nextState +", Direction: " + direction + "<br>");
 
     document.getElementById("transitionText").innerHTML = "Added Transitions: <br>" + transitions;
 
-    
-    var transitionObj = new Object();
-    transitionObj.event = eventName;
-    transitionObj.nextState = nextState;
-    transitionObj.direction = direction;
-    transitionJArray.push(transitionObj);
+    if(!duplicate) {
+      var transitionObj = new Object();
+      transitionJArray = [];
+      transitionObj.event = eventName;
+      transitionObj.nextState = nextState;
+      transitionObj.direction = direction;
+      transitionJArray.push(transitionObj);
 
-    var statesObj = new Object();
-    statesObj.stateName = stateName;
-    statesObj.transition = transitionJArray;
-    statesJArray.push(statesObj);
+      var statesObj = new Object();
+      statesObj.stateName = stateName;
+      statesObj.transition = transitionJArray;
+      statesJArray.push(statesObj);
+    } 
   } else alert("Empty parameter");
   
 }
@@ -78,8 +101,23 @@ obj.states = statesJArray;
 
 var dataContainer = document.getElementsByClassName('results__display')[0];
 dataContainer.textContent = JSON.stringify(obj, null, "  ");
+download('config_direction.json', JSON.stringify(obj));
+
 
 };
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
 
 
 var form = document.getElementsByClassName('contact-form')[0];
